900cf97f8abb55cefd6be2f77e7f7d5740a5aa9c
hcsinergia <hcsinergia@gmail.com>
2016-12-17 14:04:37 -0300

    Cambios en el modelo y en el estilo

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import  url
 
-from apps.adopcion.views import index_adopcion, SolicitudList
+from apps.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate
 
 urlpatterns = [
     url(r'^index$', index_adopcion),
-    url(r'^solicitud/listar$', Solicitud.as_view(), name='solicitud_listar'),
-
+    url(r'^solicitud/listar$', SolicitudList.as_view(), name='solicitud_listar'),
+    url(r'^solicitud/nueva$', SolicitudCreate.as_view(), name='solicitud_crear'),
 ]
+
--- refugio/urls.py
+++ refugio/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^mascota/', include ('apps.mascota.urls', namespace="mascota" )),
-    url(r'^adopcion/', include ('apps.adopcion.urls', namespace="adopcion")), 
+    url(r'^adopcion/', include ('apps.adopcion.urls', namespace="adopcion" )), 
 ]
